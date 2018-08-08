86596edaaf8b059d556c2a861a7a369000432f68
Sean Angle <seanangle@me.com>
2018-08-08 09:59:39 -0500

    fixed the start and stop on click event for the gifs

--- javascript/app.js
+++ javascript/app.js
@@ -21,67 +21,6 @@ function renderButtons() {
         // Added the button to the buttons-view div
         $("#buttons-view").append(addSport);
     }
-    $("button").on("click", function(){
-        var sport = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=25pm525lAvfypLYEB4fcUy7jZ5yd4e8U&limit=10";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-            }).then(function(response) {
-            console.log(response);
-
-            var results = response.data;
-            console.log(results.length);
-
-            // Looping over every result item
-            for (var i = 0; i < results.length; i++) {
-
-            // Only taking action if the photo has an appropriate rating
-                // Creating a div with the class "item"
-                var gifDiv = $("<div class='sport'>");
-
-                // Storing the result item's rating
-                var rating = results[i].rating;
-
-                // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Rating: " + rating.toUpperCase());
-
-                // Creating an image tag
-                var sportImage = $("<img>");
-
-                // Giving the image tag an src attribute of a proprty pulled off the
-                // result item
-                sportImage.attr("src", results[i].images.fixed_height_still.url);
-                sportImage.attr("still", results[i].images.fixed_height_still.url);
-                sportImage.attr("animated", results[i].images.fixed_height.url);
-                sportImage.attr("state", "still");
-
-
-
-                // Appending the paragraph and personImage we created to the "gifDiv" div we created
-                gifDiv.append(p);
-                gifDiv.append(sportImage);
-
-                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                $("#gif-view").prepend(gifDiv);
-            }
-
-            $("img").on("click", function(){
-                if($(this).attr("state") === "still"){
-                    $(this).attr("src", $(this).attr("animated"));
-                    $(this).attr("state", "animated");
-
-                }else{
-                    $(this).attr("src", $(this).attr("still"));
-                    $(this).attr("state", "still");
-
-                }
-
-            });
-
-        });
-    });
 }
 
 
@@ -105,3 +44,64 @@ $(".addNow").on("click", function(event) {
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
+
+$(document).on("click", "button", function(){
+    var sport = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=25pm525lAvfypLYEB4fcUy7jZ5yd4e8U&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+        console.log(response);
+
+        var results = response.data;
+        console.log(results.length);
+
+        // Looping over every result item
+        for (var i = 0; i < results.length; i++) {
+
+        // Only taking action if the photo has an appropriate rating
+            // Creating a div with the class "item"
+            var gifDiv = $("<div class='sport'>");
+
+            // Storing the result item's rating
+            var rating = results[i].rating;
+
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + rating.toUpperCase());
+
+            // Creating an image tag
+            var sportImage = $("<img>");
+
+            // Giving the image tag an src attribute of a proprty pulled off the
+            // result item
+            sportImage.attr("src", results[i].images.fixed_height_still.url);
+            sportImage.attr("still", results[i].images.fixed_height_still.url);
+            sportImage.attr("animated", results[i].images.fixed_height.url);
+            sportImage.attr("state", "still");
+
+
+
+            // Appending the paragraph and personImage we created to the "gifDiv" div we created
+            gifDiv.append(p);
+            gifDiv.append(sportImage);
+
+            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+            $("#gif-view").prepend(gifDiv);
+        }        
+    });
+});
+
+$(document).on("click", "img", function(){
+    if($(this).attr("state") === "still"){
+        $(this).attr("src", $(this).attr("animated"));
+        $(this).attr("state", "animated");
+
+    }else{
+        $(this).attr("src", $(this).attr("still"));
+        $(this).attr("state", "still");
+
+    }
+
+});
